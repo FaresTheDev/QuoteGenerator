26b0768bd5ba2b301a529e0002e761267d726fe0
Fares alharbi <faresthedev@gmail.com>
2023-04-15 06:57:11 +0300

    making functions names more understandable

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const newQuoteBtn = document.getElementById('new-quote-btn');
 const loader = document.getElementById('loader');
 
 //Show loading 
-function loading() {
+function StartLoading() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 };
@@ -15,20 +15,20 @@ function loading() {
 function completeLoading() {
     loader.hidden = true;
     quoteContainer.hidden = false;
-}
+};
 
 function newQuote () {
     //get quote, show loader
-    loading();
+    StartLoading();
 
     //pick a random quote from our database
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
     quoteAuthor.textContent = quote.author;
     quoteText.textContent = quote.text;
 
     // add class long-quote to make the font smaller
     if (quote.text.length > 120) {
-        quoteText.classList.add('long-qoute')
+        quoteText.classList.add('long-qoute');
     } else {quoteText.classList.remove('long-quote')};
 
     //hide loader
